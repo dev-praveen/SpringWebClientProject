6618c70e191767889c55c1a25bcc7febb0367e3f
Praveen Sana <spraveen0244@gmail.com>
2020-12-05 22:08:53 +0530

    Initial commit

---
+++ src/main/java/com/rest/client/SpringWebClientProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rest.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebClientProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebClientProjectApplication.class, args);
+	}
+
+}
